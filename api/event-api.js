290cd53d9fdcf4d3c65bb883ab0f0bf6d1b00cbf
manbradcalf <ben.medcalf@gmail.com>
2020-12-24 01:24:43 -0500

    update event api and web routes

--- api/event-api.js
+++ api/event-api.js
@@ -1,103 +1,69 @@
 let express = require("express");
 let router = express.Router();
-
-async function getEvents() {
-  let events;
-  await db
-    .child("events")
-    .once("value")
-    .then((eventsData) => {
-      events = eventsData;
-    });
-  return events;
-}
-
-async function getEvent(eventId) {
-  let eventResponse;
-  await db
-    .child(`events/${eventId}`)
-    .once("value")
-    .then(
-      (eventData) => {
-        if (eventData.val()) {
-          eventResponse = eventData;
-        } else {
-          eventResponse = {
-            message: `No event exists for the requested event id ${req.params.eventId}`,
-          };
-        }
-      },
-      (error) => {
-        eventResponse = error;
-      }
-    );
-  return eventResponse;
-}
-
+let eventsDBClient = require("../data/eventsDBClient.js");
+const { response } = require("express");
 /**
  * GET ALL EVENTS
  */
 router.get("/", async (req, res) => {
-  let events = await getEvents();
-  console.log(`event data is ${events}`);
-  res.send(events);
+  let events = await eventsDBClient.getEvents();
+  try {
+    if (events) {
+      res.send(events);
+    } else {
+      res.status(404).send({
+        message: `Unable to find any events...that's odd`,
+      });
+    }
+  } catch (e) {
+    res.status(500).send({
+      message: `Something went wrong looking for events:\n${e}`,
+    });
+  }
 });
 
 /**
  * GET EVENT DATA
  */
 router.get("/:eventId", async (req, res) => {
-  let event = await getEvent(req.params.eventId);
-  res.send(event);
+  let eventResponse = await eventsDBClient.getEvent(req.params.eventId);
+  if (!eventResponse.error) {
+    res.status(200).send(eventResponse);
+  } else {
+    res.status(eventResponse.status).send({ error: eventResponse.error });
+  }
 });
-
 /**
  * GET EVENT USERS
  */
-router.get("/:eventId/users", function (req, res) {
-  db.child(`events/${req.params.eventId}`)
-    .once("value")
-    .then(
-      (eventData) => {
-        if (eventData == null) {
-          res.status(404).send({
-            message: `Unable to find eventId ${req.params.eventsId}`,
+router.get("/:eventId/users", async (req, res) => {
+  let eventResponse = await eventsDBClient.getEvent(req.params.eventId);
+  if (eventResponse.error) {
+    res.status(eventResponse.status).send(eventResponse.error);
+  } else {
+    let responseBody = { host: eventResponse.host, invitees: [] };
+    let inviteeIds = Object.keys(eventResponse.users);
+
+    if (!inviteeIds) {
+      res.send(responseBody);
+    } else {
+      // construct invitees list
+      for (let i = 0; i < inviteeIds.length; i++) {
+        let userId = inviteeIds[i];
+        // TODO: make the usersDBClient so we dont touch fb here
+        db.child(`/users/${userId}`)
+          .once("value")
+          .then((userData) => {
+            let userResponseBody = userData.val();
+            userResponseBody.userId = userData.key;
+            responseBody.invitees.push(userResponseBody);
+            if (i == inviteeIds.length - 1) {
+              res.send(responseBody);
+            }
           });
-        } else {
-          // We got an event, build response object and grab userIds to iterate thru
-          let responseBody = { host: {}, attendees: [] };
-          let inviteeIds = Object.keys(eventData.val().users);
-
-          // Set host
-          responseBody.host = eventData.val().host;
-
-          // Set attendees
-          for (let i = 0; i < inviteeIds.length; i++) {
-            let userId = inviteeIds[i];
-            db.child(`/users/${userId}`)
-              .once("value")
-              .then(function (userInfo) {
-                let userResponseBody = userInfo.val();
-                userResponseBody.userId = userInfo.key;
-                responseBody.attendees.push(userResponseBody);
-
-                // We have all the users now
-                if (i === inviteeIds.length - 1) {
-                  res.send(responseBody);
-                }
-              })
-              .catch((error) => {
-                console.log(error);
-                res.status(500).send("ERROR: " + error.message);
-              });
-          }
-        }
-      },
-      (error) => {
-        console.log(error);
-        res.status(500).send(error);
       }
-    );
+    }
+  }
 });
 
-(module.exports = router), getEvents;
+module.exports = router;
--- web/routes/event.js
+++ web/routes/event.js
@@ -1,31 +1,29 @@
 let express = require("express");
 let router = express.Router();
 let handlebars = require("handlebars");
-const { getEvents } = require("../../api/event-api");
+const eventsDBClient = require("../../data/eventsDBClient.js");
 
 /**
  * GET ALL EVENTS
  */
-router.get("/", async(req, res) => {
-  let events = await getEvents();
-  res.render("events", result);
-  Object.keys(events).forEach((eventId) => {
+router.get("/", async (req, res) => {
+  let eventData = await eventsDBClient.getEvents();
+  console.log(`event Data in the API is ${JSON.stringify(eventData)} \n`)
+  Object.keys(eventData).forEach((eventId) => {
     // add a url property for easy use in template
-    events[eventId].url = `./event/${eventId}`;
+    eventData[eventId].url = `./event/${eventId}`;
+    console.log(`url for eventId ${eventId} is ${eventData[eventId].url}`)
   });
-  res.render("events", Object.entries(events));
+  console.log(`eventData with urls is now ${JSON.stringify(eventData)}`)
+  res.render("events", Object.entries(eventData));
 });
 
 /**
  * GET EVENT DATA
  */
-router.get("/:eventId", async(req, res) => {
-  db.child("events")
-    .child(req.params.eventId)
-    .once("value")
-    .then(function (snapshot) {
-      res.render("event", snapshot.val());
-    });
+router.get("/:eventId", async (req, res) => {
+  let event = await eventsDBClient.getEvent(req.params.eventId);
+  res.render("event", event);
 });
 
 module.exports = router;
