8b56fc9e1d3c169832484be454dff666ba5fd17c
manbradcalf <ben.medcalf@gmail.com>
2020-12-25 18:42:58 -0500

    make users db client

---
+++ data/usersDBClient.js
@@ -0,0 +1,44 @@
+async function getUsers() {
+  let users;
+  await db
+    .child("users")
+    .once("value")
+    .then((usersData) => {
+      users = usersData.val();
+    });
+  console.log(`users in the client is ${users}`);
+  return users;
+}
+
+async function getUser(userId) {
+  console.log(`userId requested is ${userId}`)
+  let userResponse;
+  try {
+    await db
+      .child(`users/${userId}`)
+      .once("value")
+      .then(
+        (userData) => {
+          console.log(
+            `user data in client is ${JSON.stringify(userData.val())}`
+          );
+          if (userData.val()) {
+            userResponse = userData.val();
+          } else {
+            eventResponse = {
+              error: `No user exists with userId ${userId}`,
+              status: 404,
+            };
+          }
+        },
+        (error) => {
+          userResponse = { error: error, status: 500 };
+        }
+      );
+  } catch (e) {
+    userResponse = { error: error, status: 500 };
+  }
+  return userResponse;
+}
+
+module.exports = { getUser, getUsers };
